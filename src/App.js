b73af4a0a761af97b4c9630924b856cee3866c96
Polgár András <azazdeaz@gmail.com>
2015-10-14 22:36:30 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { NICE, SUPER_NICE } from './colors';
+import GSAP from 'react-gsap-enhancer'
+require('gsap')
 
+const anim = ({target}) => {
+  return TweenMax.to(target, 1, {x: '+=123', yoyo: true, repeat: -1})
+}
+
+@GSAP()
 class Counter extends Component {
   constructor(props) {
     super(props);
@@ -14,13 +21,17 @@ class Counter extends Component {
     });
   }
 
+  componentDidMount() {
+    this.addAnimation(anim)
+  }
+
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
   render() {
     return (
-      <h1 style={{ color: this.props.color }}>
+      <h1 style={{ color: this.props.color, transform: `translate(${this.props.increment}px)` }}>
         Counter ({this.props.increment}): {this.state.counter}
       </h1>
     );
@@ -31,8 +42,8 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
+        <Counter increment={10} color={NICE} />
+        <Counter increment={50} color={SUPER_NICE} />
       </div>
     );
   }
